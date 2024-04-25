6c76fc31f1000887c09e3501cba59b77eca13738
khairunnissza <khrnzsa906@gmail.com>
2024-04-25 17:16:08 +0700

    UTS Nomor 3

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,11 @@ Route::get('/', function () {
     return view('home');
 });
 
+Route::get('produk', function () {
+    $kodeproduk = ['BRG001', 'BRG002'];
+    $namaproduk = ['Buku', 'Pena'];
+    $jenisproduk = ['Alat Tulis', 'Alat Tulis'];
+    $harga = [20000, 15000];
+    $jumlah = count($kodeproduk);
+    return view('produk', compact('kodeproduk', 'namaproduk', 'jenisproduk', 'harga', 'jumlah'));
+});
